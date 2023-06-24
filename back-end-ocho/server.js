b34f67cac678d42381a2ca2a2be0f26afba2a4d9
Adnan <adnanvandyck@gmail.com>
2023-06-24 11:43:42 -0400

    Connected to MongoDb

--- back-end-ocho/server.js
+++ back-end-ocho/server.js
@@ -1,11 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 
 //CONFIGURATION
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+    )
+
+
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
